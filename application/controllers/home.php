d26ff8dd373bb9ae6539e66b9e309741888d7f9a
Taylor David <78498434+taylor-david-dev@users.noreply.github.com>
2024-07-31 16:33:48 -0300

    Ajuste relatorio_model
    Corrigido chamada arquivo relatorio_model.php ao qual havia excluido e não foi removido a chamada.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -26,27 +26,6 @@ class Home extends CI_Controller {
         if ($this->session->userdata('logged_in')) {
             $data['user'] = $this->session->userdata('logged_in');
             $data['view'] = 'Painel/home';
-
-            $this->load->model('relatorio_model', '', TRUE);
-            
-            $dadosPagos = $this->relatorio_model->total_valor_lancamento_mes_atual_pagos($this->conta_id);
-            $data['lucroXcustoMes'] = $dadosPagos;
-            
-            $dados = $this->relatorio_model->total_valor_lancamento($this->conta_id);
-            $data['lucroXcusto'] = $dados['vLista'];
-            
-            $dadosNext = $this->relatorio_model->total_valor_lancamento_proximo_mes($this->conta_id);
-            
-            
-            $data['lucroXcustoNext'] = $dadosNext['vLista'];
-            
-            $dadosYear = $this->relatorio_model->total_valor_lancamento_anual($this->conta_id);
-            $data['lucroXcustoYear'] = $dadosYear['vLista'];
-            
-            
-            $dadosDetalhadosYear = $this->relatorio_model->total_detalhado_por_ano($this->conta_id);
-            $data['lancamentosAnuais'] = $dadosDetalhadosYear['vLista'];
-            
             $this->load->view('Painel/index', $data);
         } else {
             //If no session, redirect to login page
